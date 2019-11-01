2582379843e98c3cb595bfaf4f2f587dd1ccb65a
luca <ritz.luca@gmail.com>
2019-11-01 15:59:32 +0100

    Keycloak for java

--- java/src/main/java/ticketrecognition/web/TestResource.java
+++ java/src/main/java/ticketrecognition/web/TestResource.java
@@ -4,24 +4,34 @@ import ticketrecognition.Role;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("/")
 @RolesAllowed({Role.AD_CODE, Role.SCANNER_CODE})
-@Stateless
 public class TestResource {
 
-    @Inject
+    @EJB
     TestService service;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public TestClass getTest() {
         return service.testObj();
+    }
+
+    @Path("/context")
+    @GET
+    @PermitAll
+    public String getRoles(@Context SecurityContext securityContext) {
+        String value = "name: " + securityContext.getUserPrincipal().getName() + "\n";
+        value += "is in admin role: " + securityContext.isUserInRole(Role.AD_CODE) + "\n";
+        value += "is in scanner role: " + securityContext.isUserInRole(Role.SCANNER_CODE);
+        return value;
     }
 }
--- java/src/main/java/ticketrecognition/web/TestService.java
+++ java/src/main/java/ticketrecognition/web/TestService.java
@@ -1,8 +1,11 @@
 package ticketrecognition.web;
 
+import javax.ejb.Stateless;
+
+@Stateless
 public class TestService {
 
-    TestClass testObj() {
+    public TestClass testObj() {
         TestClass test = new TestClass();
         test.setId(123L);
         test.setTest("TEST TEST");
